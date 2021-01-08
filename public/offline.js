392f5342a54bd976967b34c31f33190cf17d69c3
Andrew <mrbondmustdie@gmail.com>
2021-01-08 19:30:44 +0800

    Data now stores in indexedDB

--- public/offline.js
+++ public/offline.js
@@ -1,18 +1,61 @@
-function saveRecord(data){
+// CREATE OFFLINE DATABASE
+let db;
+const request = window.indexedDB.open("offlineBudget", 1);
 
-    console.log("REACHED SAVE RECORD");
+// CREATE SCHEMA
+request.onupgradeneeded = event => {
+    const db = event.target.result;
+    db.createObjectStore("offlineBudget", { autoIncrement: true });
+};
+
+// ON SUCCESS
+request.onsuccess = function (event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+// ON ERROR
+request.onerror = function (event) {
+    console.log(`Error: ${event.target.errorCode}`);
+};
+
+// SAVE RECORD FUNCTION
+function saveRecord(data) {
+    const transaction = db.transaction(["offlineBudget"], "readwrite");
+    const store = transaction.objectStore("offlineBudget");  
+    store.add(data);
 }
 
-const request = window.indexedDB.open("offline-budget", 1);
+// CHECK DATABASE CONNECTION
+function checkDatabase() {
+    
+  const transaction = db.transaction(["offlineBudget"], "readwrite");
+  const store = transaction.objectStore("offlineBudget");
+  const getAll = store.getAll();
 
-// // Create schema
-// request.onupgradeneeded = event => {
-//   const db = event.target.result;
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => response.json())
+      .then(() => {
+        
+        const transaction = db.transaction(["offlineBudget"], "readwrite");
+        const store = transaction.objectStore("offlineBudget");
+        store.clear();
+      });
+    }
+  };
+}
 
-//   // Creates an object store with a listID keypath that can be used to query on.
-//   const toDoListStore = db.createObjectStore("toDoList", {
-//     keyPath: "listID"
-//   });
-//   // Creates a statusIndex that we can query on.
-//   toDoListStore.createIndex("statusIndex", "status");
-// };
+// CHECK DATABASE ONLINE
+window.addEventListener("online", checkDatabase);
