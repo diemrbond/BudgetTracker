b4bce94a7227aa3190089da5c7ec4c31c0fd891d
andrew <mrbondmustdie@gmail.com>
2021-01-08 16:02:58 +0800

    transferring

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
 router.post("/api/transaction", ({body}, res) => {
+  console.log("RESPONSE STATUS: "+res.status)
   Transaction.create(body)
     .then(dbTransaction => {
       res.json(dbTransaction);
